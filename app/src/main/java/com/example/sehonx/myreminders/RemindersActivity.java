d1a39c8ef246a8d8caf6092525f2cf1f227e1684
Pawel Jaskula <pj.jaskula@gmail.com>
2017-05-16 19:38:29 +0200

    Adds new reminder and exit menu options

---
+++ app/src/main/java/com/example/sehonx/myreminders/RemindersActivity.java
@@ -0,0 +1,50 @@
+package com.example.sehonx.myreminders;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class RemindersActivity extends AppCompatActivity {
+    private ListView mListView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminders);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mListView = (ListView) findViewById(R.id.reminders_list_view);
+        //the arrayAdapter is the controller in our model-view-controller relationship.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,R.layout.reminders_row,R.id.row_text,new String[]{"first record","second record","third record"});
+        mListView.setAdapter(arrayAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_reminders, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_new:
+//create new Reminder
+                Log.d(getLocalClassName(),"create new Reminder");
+                return true;
+            case R.id.action_exit:
+                finish();
+                return true;
+            default:
+                return false;
+        }
+    }
+}
